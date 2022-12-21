d646bddf477a2a3d9dc3eeed4eaa4411888176c3
Pratham Choudhary <choudharyy.pratham@gmail.com>
2022-12-21 10:04:36 +0530

    First Bad Version (Leetcode)
    Working Code

---
+++ Assignment - Binary Search/FirstBadVersion.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int ans=0;
+        long long low=0, high=n;
+        while(low<=high)
+        {
+            long long mid=(low+high)/2;
+            if(isBadVersion(mid))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
