e4a5b2b97eb4150fa5089544dcda4e5ce8c94691
Pratham Choudhary <choudharyy.pratham@gmail.com>
2022-12-23 19:33:08 +0530

    Playing With Characters (HackerRank)
    Working Code

---
+++ Assignment - 2 (Intro to C)/PlayingWithCharacter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char a[10],b[100],c[100];
+    gets(a);
+    gets(b);
+    gets(c);
+    printf("%s\n%s\n%s",a,b,c);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
