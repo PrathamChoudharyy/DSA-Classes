6c4de728c6fc4e2256f3666443d31a4962f18dcf
Pratham Choudhary <choudharyy.pratham@gmail.com>
2022-12-23 15:07:48 +0530

    Function (HackerRank)
    Working Code

---
+++ Assignment - 1/Functions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d)
+{
+    int max=0;
+    int arr[4]={a,b,c,d};
+    for(int i=0; i<4;i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
