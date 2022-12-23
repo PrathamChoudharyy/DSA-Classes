beb63b9c7a3fb4b35cdedf4c19974bce53e9798f
Pratham Choudhary <choudharyy.pratham@gmail.com>
2022-12-23 14:53:34 +0530

    Array Introduction - Reversing (HackerRank)
    Working Code

---
+++ Assignment - 1/Array.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0 ; i<n ; i++)
+    {
+        cin>>arr[i];
+    }   
+    for(int j=n-1 ; j>=0 ; j--)
+    {
+        cout<<arr[j]<<" ";
+    }
+    return 0;
+}
