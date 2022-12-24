eae82010c323a9d6df5db171171a9aa2ec5f257d
Pratham Choudhary <choudharyy.pratham@gmail.com>
2022-12-24 22:02:38 +0530

    Function in C (HackerRank)
    Working Code

---
+++ Assignment - 2 (Intro to C)/Function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a,int b,int c,int d)
+{
+    int arr[4]={a,b,c,d};
+    int max=0;
+    for(int i=0; i<4 ; i++)
+    {
+        if(max<=arr[i])
+        {
+            max=arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
