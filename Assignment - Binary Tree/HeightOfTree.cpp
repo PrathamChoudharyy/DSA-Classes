59f1908e27741fa071fdefda1c40802ab36e37df
Pratham Choudhary <choudharyy.pratham@gmail.com>
2023-01-25 16:16:22 +0530

    Height Of Tree (Hackerrank)
    Working Code

---
+++ Assignment - Binary Tree/HeightOfTree.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        if(root == NULL)    return 0;    
+        int x=0;
+        int y=0;
+        if(root->left != NULL)
+        {
+            x=height(root->left);
+            x++;
+        }
+        if(root->right != NULL)
+        {
+            y=height(root->right);
+            y++;
+        }
+        return max(x,y);
+    }
+
+}; //End of Solution
